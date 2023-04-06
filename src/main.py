dfebd493dfb4277a958b1efc1e9b2156ca72cb74
Ximer <1178890320@qq.com>
2023-04-06 23:40:06 +0800

    rewrite unet; switch img to tenser

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from LEVIRCDDataset import LEVIRCDDataset, Mode
+from datasets.LEVIRCDDataset import LEVIRCDDataset, RunningMode
 import PIL.Image as Image
 import torch
 
-img1, img2=LEVIRCDDataset(mode=Mode.train).__getitem__(0)
+img1, img2=LEVIRCDDataset(mode=RunningMode.train).__getitem__(0)
 img1.show()
